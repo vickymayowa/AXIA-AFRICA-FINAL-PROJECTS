199638e59a54a3a98bb906627770b0aeb70c44c9
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2024-10-09 14:23:22 +0100

    Updated db.js to use environment variable for MongoDB URI, refactored index.js to include error handling and added mongoose dependency to package.json

--- Task Management Application/config/db.js
+++ Task Management Application/config/db.js
@@ -1,17 +1,14 @@
-const mongoose require("mongoose";)
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     const mongoURI =
-      process.env.NODE_ENV === "production"
-        ? process.env.MONGO_LIVE_URI
-        : process.env.MONGO_URI;
+      process.env.MONGO_URI || "mongodb://127.0.0.1:27017/task_web_app";
     const conn = await mongoose.connect(mongoURI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
   }
 };
 
-module export connectDB;
+module.exports = connectDB;
--- Task Management Application/index.js
+++ Task Management Application/index.js
@@ -1,9 +1,27 @@
-const express = require("express")
-const app = express()
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const morgan = require("morgan");
+const connectDB = require("./config/db.js");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware.js");
+const PORT = process.env.PORT || 4000;
 
+connectDB();
+const app = express();
 
-const PORT = 3400
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT,()=>{
-    console.log(`Server started on ${PORT}`)
-})
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Welcome to my Task Management Application server!" });
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
